89b9621b81466351b9a0bd042237972f5ee71ff2
rohithchunduri <rohith.chunduri@gmail.com>
2020-08-25 13:20:50 +0530

    Create PalindromicSubstring.java

---
+++ DynamicProgramming/PalindromicSubstring.java
@@ -0,0 +1,41 @@
+class Solution {
+    /*
+     Count number of palindromic substrings.
+    */
+    
+    public int countSubstrings(String s) {
+        
+        int n = s.length();
+        if(n == 0){
+            return 0;
+        }
+        boolean dp[][] = new boolean[n][n];
+        
+        int count = 0;
+        for(int i = 0 ; i < n; i++){
+            dp[i][i] = true;
+            count += 1;
+        }
+        
+        for(int i = 0 ; i < n-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                dp[i][i+1] = true;
+                count += 1;
+            }
+        }
+        
+        for(int len = 3; len <= n; len++){
+            for(int i = 0 ; i + len - 1 < n; i++){
+                int j = i + len -1;
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1];
+                }
+                if(dp[i][j] == true){
+                    count += 1;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
